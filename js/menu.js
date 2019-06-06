8262da28f103d558180aebb58fc6293924299075
bruinen <max.machnik@gmail.com>
2019-06-06 23:00:19 +0200

    80% done

--- js/menu.js
+++ js/menu.js
@@ -1,7 +1,9 @@
 const toggleMenu = () => {
     const menuCont = document.querySelector('.menu');
     const menuToggler = document.querySelector('.hamburger');
+    const cover = document.querySelector('.cover');
     menuCont.classList.toggle('menu--active');
     menuToggler.classList.toggle('is-active');
+    cover.classList.toggle('cover--hidden');
     console.log('click')
 }
