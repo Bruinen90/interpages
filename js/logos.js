0f18d06c71ff8b3634a7ff7307cd1a8e3fdf6ad1
bruinen <max.machnik@gmail.com>
2019-07-25 21:08:38 +0200

    Logos slider for mobile added

--- js/logos.js
+++ js/logos.js
@@ -1,1 +1,24 @@
-const logosCont = document.querySelector('.logos');
+if (window.innerWidth < 768) {
+	const logosCont = document.querySelector(".logos__cont");
+	const allLogos = document.querySelectorAll(".logos__img");
+	let currLogo = 0;
+	const changeLogo = direction => {
+        let nextLogo = currLogo + direction;
+        console.log(nextLogo)
+		if (nextLogo < allLogos.length && nextLogo >= 0) {
+			currLogo = nextLogo;
+			logosCont.style.transform = `translateX(${40 * -currLogo}vw`;
+		} else if (nextLogo === allLogos.length) {
+            logosCont.style.transform = `translateX(0)`;
+            currLogo = 0;
+        }
+	};
+	const logosSwipeHandler = new Hammer(logosCont);
+	logosSwipeHandler.on("swipeleft", function(ev) {
+		changeLogo(1);
+	});
+	logosSwipeHandler.on("swiperight", function(ev) {
+		changeLogo(-1);
+	});
+	window.setInterval(() => changeLogo(1), 1000);
+}
